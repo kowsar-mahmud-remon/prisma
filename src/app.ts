e428337f632a900ddb60751029bbe589c2c0211f
creativerze <creativerze@gmail.com>
2023-08-15 19:18:09 +0600

    added create category

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { UserRoutes } from "./modules/user/user.route";
+import { CategoryRoutes } from "./modules/category/category.route";
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/user/", UserRoutes);
+app.use("/api/v1/user/", UserRoutes);
+app.use("/api/v1/category/", CategoryRoutes);
 
 export default app;
