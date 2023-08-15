22000ca916ea96a10b31e3c7631e3dcda44bae6c
creativerze <creativerze@gmail.com>
2023-08-16 00:22:43 +0600

    added getAllPost

---
+++ src/modules/post/post.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { PostService } from "./post.service";
+
+const createPostController = async (req: Request, res: Response) => {
+  try {
+    const result = await PostService.createPost(req.body);
+
+    res.send({
+      success: true,
+      message: "Post Created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const getAllPostController = async (req: Request, res: Response) => {
+  try {
+    const result = await PostService.getAllPost();
+
+    res.send({
+      success: true,
+      message: "Post fetch successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export const PostController = {
+  createPostController,
+  getAllPostController,
+};
---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,30 @@
+import { Post, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createPost = (data: Post): Promise<Post> => {
+  const result = prisma.post.create({
+    data,
+    include: {
+      author: true,
+      category: true,
+    },
+  });
+
+  return result;
+};
+
+const getAllPost = async () => {
+  const result = await prisma.post.findMany({
+    include: {
+      author: true,
+      category: true,
+    },
+  });
+  return result;
+};
+
+export const PostService = {
+  createPost,
+  getAllPost,
+};
